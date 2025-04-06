2ef5bf94059313bb5e34d3c885baca8f51d47a43
krishna jadhav <jadhavkrishna475@gmail.com>
2025-04-06 09:15:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,22 +10,27 @@ dotenv.config({ path: "./config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: "https://unique-zabaione-8f246c.netlify.app", // ✅ CORS fixed
     methods: ["POST"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/reservation", reservationRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "HELLO WORLD AGAIN"
-})})
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD AGAIN",
+  });
+});
 
 dbConnection();
 
 app.use(errorMiddleware);
 
 export default app;
+
